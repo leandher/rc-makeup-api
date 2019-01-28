becd2378f5cfaf1d0e772282f55d02a9305f8d50
leandher <leandher_bessa@hotmail.com>
2019-01-28 18:20:06 -0300

    chore(firebase): config firebase deploy

--- src/navigation/Products/components/index.js
+++ src/navigation/Products/components/index.js
@@ -1,8 +1,5 @@
 import Colors from './Colors';
 import Filter from './Filter';
 import Image from './Image';
-import Rating from './Rating';
 
-export {
-  Colors, Filter, Image, Rating,
-};
+export { Colors, Filter, Image };
